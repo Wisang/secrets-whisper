6e68aae11d5444a79f3ab9c5b4f31113b9131f9b
wisang <wseom7@gmail.com>
2022-10-01 13:22:13 +0900

    res.send working

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 //jshint esversion:6
+const express = require('express');
+const bodyParser = require('body-parser')
+const ejs = require('ejs');
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.get('/', function(req, res) {
+  res.send("<h1>Working?</h1>");
+});
+
+app.listen(3000, function() {
+  console.log('Express server listening on port 3000');
+});
