dcc98445aa01607b77c75cb06a2812e5e3c651b4
wisang <wseom7@gmail.com>
2022-10-02 11:58:32 +0900

    register and login implemented

--- app.js
+++ app.js
@@ -33,6 +33,41 @@ app.get('/register', function(req, res) {
   res.render("register");
 });
 
+app.post('/register', function(req, res) {
+  const newUser = User({
+    email: req.body.username,
+    password: req.body.password
+  });
+
+  newUser.save(function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("secrets");
+    }
+  });
+});
+
+app.post('/login', function(req, res) {
+  User.findOne({
+    email: req.body.username
+  }, function(err, user) {
+    if (err) {
+      res.send(err);
+    } else {
+      if (user) {
+        if (user.password !== req.body.password) {
+          res.send('incorrect password');
+        } else {
+          res.render("secrets");
+        }
+      } else {
+        res.send('user not found');
+      }
+    }
+  });
+});
+
 app.listen(3000, function() {
   console.log('Express server listening on port 3000');
 });
